ac88f2ee131e1f7de0d73e999af629090f7aecb0
Nurmukhanov <olzhas.nurmukhanov@gmail.com>
2017-06-06 21:56:48 +0600

    all

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,84 @@
-import javax.bluetooth.DiscoveryAgent;
-import javax.bluetooth.LocalDevice;
-import javax.bluetooth.RemoteDevice;
-import java.io.IOException;
+import de.serviceflow.codegenj.ObjectManager;
+import org.bluez.Adapter1;
+import org.bluez.Device1;
+import org.bluez.GattCharacteristic1;
+import org.bluez.GattService1;
 
-/**
- * Created by olzhas on 06.06.17.
- */
+import java.io.IOException;
+import java.util.List;
+import java.util.logging.Level;
+
 public class Main {
-    public static void main(String[] args) throws IOException {
-        RemoteDevice[] rdList = LocalDevice.getLocalDevice().getDiscoveryAgent().retrieveDevices(DiscoveryAgent.PREKNOWN);
-        for (int i=0; i<rdList.length; i++){
-            System.out.println(rdList[i].getBluetoothAddress());
-            System.out.println(rdList[i].getFriendlyName(true));
+    public static void main(String[] args) throws InterruptedException {
+
+        ObjectManager m = ObjectManager.getInstance();
+        ObjectManager.getLogger().setLevel(Level.FINE);
+
+        // Show what's on the bus:
+        m.dump();
+
+        List<Adapter1> adapters = m.getAdapters();
+        System.out.println(" ==> # = " + adapters.size());
+
+        // Find our bloetooth adapter, and start Discovery ...
+        Adapter1 defaultAdapter = null;
+        for (Adapter1 a : adapters) {
+            System.out.println(" ==> Adapter: " + a.getName());
+            try {
+                a.startDiscovery();
+            } catch (IOException e) {
+                System.out.println(" ... ignored.");
+                continue;
+            }
+            defaultAdapter = a;
+        }
+        if (defaultAdapter == null) {
+            System.out.println("no useable adapter found. Exit.");
+            System.exit(1);
+        }
+
+        // Wait for devices to be discovered
+        Thread.sleep(5000);
+
+        Adapter1 a = defaultAdapter;
+        for (Device1 d : a.getDevices()) {
+            if ("CC2650 SensorTag".equals(d.getName())) {
+                System.out.println(" ==> Device: " + d.getName());
+                try {
+                    if (!d.getConnected()) {
+                        d.connect();
+                        System.out.println(" ... connected.");
+                    } else {
+                        System.out.println(" ... already connected.");
+                    }
+                } catch (IOException e) {
+                    System.out.println(" ... ignored: " + e.getMessage());
+                }
+            } else {
+                System.out.println(" --> Device " + d.getName() + " skipped.");
+            }
+        }
+
+        // Use the API to traverse through the tree.
+        System.out.println("*** Object Tree:");
+        for (Adapter1 a1 : adapters) {
+            System.out.println(" ... adapter " + a.getObjectPath() + "  " + a.getName());
+            for (Device1 d : a.getDevices()) {
+                System.out.println("  .. device " + d.getObjectPath() + "  " + d.getName());
+                for (GattService1 s : d.getServices()) {
+                    System.out.println("   . service " + s.getObjectPath() + "  " + s.getUUID());
+                    for (GattCharacteristic1 c : s.getCharacteristics()) {
+                        System.out.println("    . char " + c.getObjectPath() + "  " + c.getUUID());
+                    }
+                }
+            }
+        }
+
+        try {
+            defaultAdapter.stopDiscovery();
+            System.out.println(" ... stopped.");
+        } catch (IOException e) {
+            System.out.println(" ... ignored.");
         }
     }
 }
